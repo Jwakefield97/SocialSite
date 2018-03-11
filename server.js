97645e2f0e029f78bfe1ec985e4bc21dd0b1edd5
jake <jacobstevenwakefield@gmail.com>
2018-03-10 18:21:59 -0600

    wire up login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict"; 
 const express = require("express"), 
     app = express(), 
+    bodyParser = require("body-parser"), 
     http = require("http").Server(app),
     socketIo = require("socket.io")(http); 
 app.use(express.static("resources"));
@@ -8,8 +9,16 @@ app.use(express.static("resources"));
 app.set("view engine","pug");//sets view engine  
 app.set("views", "./views");  //tells view engine where to look for templates 
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
 app.get("/",(req,res)=>{
     res.render("login.pug"); 
+}); 
+
+app.post("/login-attempt",(req,res)=>{
+    console.log(req.body); 
+    res.send("success"); 
 }); 
 
 socketIo.on("connection",(socket)=>{
