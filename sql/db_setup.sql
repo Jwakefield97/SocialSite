a40708dda7f98859d7ac49910f1262dcc420cbd7
jake <jacobstevenwakefield@gmail.com>
2018-03-13 22:56:29 -0500

    add friend tables and additional info

--- sql/db_setup.sql
+++ sql/db_setup.sql
@@ -4,10 +4,33 @@
 	Id INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(200) NOT NULL,
     LastName VARCHAR(200) NOT NULL,
-    Email VARCHAR(300),
     Username VARCHAR(200) NOT NULL, 
     Password VARCHAR(200) NOT NULL, 
     DateJoined DATETIME DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY (Id) 
 ); */
 
+/*CREATE TABLE user_additional (
+	username VARCHAR(200) NOT NULL,
+    profileImage VARCHAR(500),
+    bio VARCHAR(2000), 
+	email VARCHAR(300),
+    phone_number INT, 
+    last_accessed DATETIME, 
+    PRIMARY KEY (username) 
+); */ 
+
+/*CREATE TABLE friend (
+	username VARCHAR(200) NOT NULL,
+    friend_username VARCHAR(200) NOT NULL,
+	time_friended DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (username) 
+); */
+
+/*CREATE TABLE friends_pending (
+	username VARCHAR(200) NOT NULL,
+    friend_username VARCHAR(200) NOT NULL,
+    reject BOOL, 
+    time_requested DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY (username) 
+); */ 
